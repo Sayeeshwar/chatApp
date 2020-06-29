502ae57cdeba3c838f13d04878de352cf31ca2d2
Sayeeshwar <sayeeshwar.girish@gmail.com>
2020-06-29 17:48:46 +0530

    Define addUser function

--- server/users.js
+++ server/users.js
@@ -2,7 +2,19 @@ const users=[]
 
 const addUser=({id,name,room})=>{
 
-    
+    name=name.trim().toLowerCase();
+    room=room.trim().toLowerCase();
+
+    const existingUser= users.find((user)=> user.name==name && user.room==room);
+
+    if(existingUser)
+    {
+        return {error:'Username is taken!'}
+    }
+    const user ={name,id,room};
+    users.push(user);
+    return {user}
+
 }
 
 const removeUser= () => {
