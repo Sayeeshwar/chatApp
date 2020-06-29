34bd7aafd380e2054c0e82c189842be0b99a203c
Sayeeshwar <sayeeshwar.girish@gmail.com>
2020-06-29 17:07:21 +0530

    Get name and room values from query string in Chat component

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 import './Chat.css'
-const Chat= () => {
+
+const Chat= ({location}) => {
+    const [name,setName]=useState('');
+    const [room,setRoom]=useState('');
+    useEffect(()=>{
+        const {name,room}=queryString.parse(location.search);
+        console.log(name,room);
+        setName(name);
+        setRoom(room)
+    })
+
     return(
         <div>
             <h1>Heyy! welcome to chat</h1>
